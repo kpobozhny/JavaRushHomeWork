bf6c4ddc548b234bc2428d24735993ffccbcaddd
kpobozhny <k.pobozhny@gmail.com>
2016-06-29 00:16:26 +0300

    level10.lesson11

---
+++ src/com/javarush/test/level10/lesson11/home05/Solution.java
@@ -0,0 +1,62 @@
+package com.javarush.test.level10.lesson11.home05;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* Количество букв
+Ввести с клавиатуры 10 строчек и подсчитать в них количество различных букв (для 33 букв алфавита).  Вывести результат на экран.
+Пример вывода:
+а 5
+б 8
+в 3
+г 7
+д 0
+…
+я 9
+*/
+
+public class Solution
+{
+    public static void main(String[] args)  throws Exception
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        //алфавит
+        String abc = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        char[] abcArray = abc.toCharArray();
+
+        ArrayList<Character> alphabet = new ArrayList<Character>();
+        for (int i = 0; i < abcArray.length; i++)
+        {
+            alphabet.add(abcArray[i]);
+        }
+
+        //ввод строк
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < 10; i++)
+        {
+            String s = reader.readLine();
+            list.add(s.toLowerCase());
+
+        }
+
+        int count=0;
+        //напишите тут ваш код
+        for (Character ch : alphabet)
+        {
+            for (String elem : list)
+            {
+                char[] listArray = elem.toCharArray();
+                for (int i=0; i<listArray.length; i++){
+                    if (listArray[i]==ch){
+                        count++;
+                    }
+                }
+            }
+            System.out.println(ch +" "+count);
+            count=0;
+        }
+    }
+
+}
