23152b451f959fb852ce7d08b0144cd5f7f5a235
kpobozhny <k.pobozhny@gmail.com>
2016-09-29 10:27:18 +0300

    Level 14 -lesson 08 - started

---
+++ src/com/javarush/test/level14/lesson08/home02/BubblyWine.java
@@ -0,0 +1,11 @@
+package com.javarush.test.level14.lesson08.home02;
+
+/**
+ * Created by kostya on 9/27/16.
+ */
+public class BubblyWine extends Wine
+{
+    public String getHolidayName(){
+        return "Новый год";
+    }
+}
---
+++ src/com/javarush/test/level14/lesson08/home02/Drink.java
@@ -0,0 +1,11 @@
+package com.javarush.test.level14.lesson08.home02;
+
+/**
+ * Created by kostya on 9/27/16.
+ */
+public abstract class Drink
+{
+    public void taste(){
+        System.out.println("Вкусно");
+    }
+}
