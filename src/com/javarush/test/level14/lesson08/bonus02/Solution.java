563f60110fe3264bdf318f460e0f16ef1cc27af4
kpobozhny <k.pobozhny@gmail.com>
2016-10-05 01:11:15 +0300

    Level 14 -bonus02 - completed

---
+++ src/com/javarush/test/level14/lesson08/bonus02/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.test.level14.lesson08.bonus02;
+
+/* НОД
+Наибольший общий делитель (НОД).
+Ввести с клавиатуры 2 целых положительных числа.
+Вывести в консоль наибольший общий делитель.
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Integer a = Integer.parseInt(reader.readLine());
+        Integer b = Integer.parseInt(reader.readLine());
+        //Euclidean algorithm
+        int nod = 1;
+
+
+            while(!(a==b)){
+                if(a>b) a=Math.abs(a-b); else b=Math.abs(a-b);
+            }
+            nod=a;
+
+
+        System.out.println(nod);
+    }
+
+}
